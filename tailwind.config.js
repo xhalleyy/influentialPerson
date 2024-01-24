6f7b66c9ff69fa1ae7eec52426831457775a014d
xhalleyy <xhalleyy@gmail.com>
2024-01-24 01:30:12 -0800

    corrected and added text

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,7 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    // screens: {
-    //   'tablet': '640px',
-    //   // => @media (min-width: 640px) { ... }
-
-    //   'laptop': '1024px',
-    //   // => @media (min-width: 1024px) { ... }
-
-    //   'desktop': '1280px',
-    //   // => @media (min-width: 1280px) { ... }
-    // },
+    
     extend: {},
   },
   plugins: [
