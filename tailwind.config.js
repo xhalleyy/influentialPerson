80af19180349a816eb2b46cedea8caa165d0599c
xhalleyy <xhalleyy@gmail.com>
2024-01-24 00:19:10 -0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    'node_modules/preline/dist/*.js',
+    "./*.{html,js}",
+    "./pages/**/*.html",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    // screens: {
+    //   'tablet': '640px',
+    //   // => @media (min-width: 640px) { ... }
+
+    //   'laptop': '1024px',
+    //   // => @media (min-width: 1024px) { ... }
+
+    //   'desktop': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+    require('flowbite/plugin'),
+  ],
+}
+
